3b26c881a3497ae990054d248597735a4e884077
khailla <2522500069@mahasiswa.atmaluhur.ac.id>
2025-11-12 14:55:52 +0700

    menambahkan session ke index.php

--- pertemuan-07/proses.php
+++ pertemuan-07/proses.php
@@ -1,5 +1,14 @@
 <?php 
-echo $_POST["txtNama"];
-echo $_POST["txtEmail"];
-echo $_POST["txtPesan"];
+session_start(); 
+
+$akunama = $_POST["txtNama"];
+$akuemail = $_POST["txtEmail"];
+$akupesan = $_POST["txtpesan"];
+
+$_SESSION["txtNama"] = $akunama;
+$_SESSION["txtEmail"] = $akuemail;
+$_SESSION["txtPesan"] = $akupesan;
+
+
+header("location: index. php");
 ?>
