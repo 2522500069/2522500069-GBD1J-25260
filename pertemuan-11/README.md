3cf9e0c4c77f1bd3e12119f5320f98638fdece64
khailla <2522500069@mahasiswa.atmaluhur.ac.id>
2025-12-15 21:21:18 +0700

    menambahkan kode baru ke proses.php

--- pertemuan-11/README.md
+++ pertemuan-11/README.md
@@ -8,5 +8,6 @@ Pada hari kamis 11 desember saya 2025 belajar:
 <li>menambah flash message dari session ke index.php</li>
 <li>menambahkan kode baru ke flash massage ke idex.php</li>
 <li>menambahkan kode baru di index.php</li>
+<li>menambahkan kode ke proses.php</li>
 
 
--- pertemuan-11/proses.php
+++ pertemuan-11/proses.php
@@ -1,6 +1,75 @@
 <?php
 session_start();
 
+if ($_SERVER['REQUEST_METHOD'] !== 'POST') {
+  $_SESSION['flash_error'] = 'akses tidak valid. ';
+  redirect_ke('index.php#contact');
+}
+
+$nama  = bersihkan($_POST['textNama'] ?? '');
+$email = bersihkan($_POST['textEmail'] ?? '');
+$pesan = bersihkan($_POST['textPesan'] ?? '');
+
+#validasi sederhana $nama 
+$errors = []; #ini array untuk menampung semua error yang ada 
+
+if ($nama === '') {
+  $errors[] = 'Nama wajib diisi.';
+}
+
+if ($email === '') {
+  $errors[] = 'Email wajib diisi.';
+} elseif (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+  $errors[] = 'format e-mail tidak valid,';
+}
+
+if ($pesan === '') {
+  $errors[] = 'pesan wajib diisi'
+}
+
+
+if (empty($errors)) {
+ $_SESSION['old'] =[
+  'nama'  => $nama,
+  'email' => $email,
+  'pesan' => $pesan,
+ ];
+
+ $_SESSION['flash_error'] = implode('<br>' , $errors);
+ redicert_ke('index.php#contact');
+}
+
+$aql = "INSERT INTO tbl_tamu (cnama, cemail, cpesan) VALUES (?, ?, ?)";
+$stmt = mysqli_prepare($conn, $sql):
+
+if (!$atmt) {
+  $_SESSION['flash_eror'] = 'terjadi kesalahan sistem (prepare gagal).';
+  redirect_ke('index.php#contact');
+}
+
+mysqli_stmt_bind_param($stmt, "sss", $nama, $email $pesan);
+
+if (mysqli_stmt_bind_param($stmt)) {
+  unset($_SESSION['old']);
+  $_SESSION['flash_sukses'] = 'terima kasih, data Anda sudah tersimpan.';
+  redicert_ke('index.php#contact');
+ } else {
+  $_SESSION['old'] =[
+     'nama'  => $nama,
+     'email' => $email,
+     'pesan' => $pesan 
+  ];
+  $_SESSION['flash_error'] = 'Data gagal disimpan. silakan coba lagi.';
+  redicert_ke('index.php#contact');
+}
+  
+mysqli_stmt_close($stmt);
+
+$arrContact = [
+  
+]
+
+
 $arrContact = [
   "nama" => $_POST["txtNama"] ?? "",
   "email" => $_POST["txtEmail"] ?? "",
