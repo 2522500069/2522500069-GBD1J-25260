0b64114572dfcc9b2e6e6290c888b436462e316d
khailla <2522500069@mahasiswa.atmaluhur.ac.id>
2025-12-18 15:11:03 +0700

    mencopy file pertemuan-11 ke folder pertemuan-12

---
+++ pertemuan-12/README.md
@@ -0,0 +1,6 @@
+# pertemuan-12
+Khailla  2522500069
+Pada hari kamis 18 desember saya 2025 belajar:
+
+<ol>
+<li>mencopy file pertemuan - 11 ke folder pertemuan-12</li>
---
+++ pertemuan-12/read.php
@@ -0,0 +1,31 @@
+<?php
+session_start();
+require 'koneksi.php';
+require 'fngsi.php';
+
+$sql       = "SELECT * FROM tbl_tamu ORDER BY cid DESC";
+$q         = mysqli_query($conn, $sql);
+$no      = 1;
+?>
+<table border="1" cellpadding="8" cellspacing="0">
+    <tr>
+        <th>No</th>
+        <th>ID</th>
+        <th>Nama</th>
+        <th>Email</th>
+        <th>Pesan</th>
+        <th>Created At</li>
+    </tr>
+    
+    <?php while ($row = mysqli_fetch_assoc($q)): ?>
+    <tr>
+        <td><?= $no++; ?></td>
+        <td><a= href="edit.php?cid=<?= (int)$row['cid']; ?>">edit</a></td>
+        <td><?= $row['cid']; ?></td>
+        <td><?= htmlspecialchars($row['cnama']); ?></td>
+        <td><?= htmlspecialchars($row['cemail']); ?></td>
+        <td><?= nl2br(htmlspecialchars($row['cpesan'])); ?></td>
+        <td><?= htmlspecialchars($row['dcreated_at']); ?></td>
+    </tr>
+    <?php endwhile; ?>
+</table>
